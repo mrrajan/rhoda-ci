7b611392bee43ec23de476d6805b7d7a7aa3cbb1
Harsh Kumar <hakumar@redhat.com>
2022-04-29 15:19:47 +0530

    Defaulted Test Case File to Test Cases Directory. Can be overwritten with -t flag
    Signed-off-by: Harsh Kumar <hakumar@redhat.com>

--- run_robot_test.sh
+++ run_robot_test.sh
@@ -3,7 +3,7 @@ set -eux
 
 # -------- Global Variables --------
 TEST_CASE_LOCATION=tests
-TEST_CASE_FILE=""
+TEST_CASE_FILE=tests
 TEST_VARIABLES_FILE=test-variables.yaml
 TEST_VARIABLES=""
 TEST_ARTIFACT_DIR="test-output"
@@ -85,7 +85,7 @@ disp_usage() {
     echo "      -i <TEST_INCLUDE_TAG>: Specify included tags for Robot"
     echo "      -r <EXTRA_ROBOT_ARGS>: Additional arguments to pass to the robot cli"
     echo "      -s: Skip the installation of required Python Libraries"
-    echo "      -t <TEST_CASE_FILE>: Specify test case to run, should be present under Tests directory"
+    echo "      -t <TEST_CASE_FILE>: Specify test case to run, should be present under tests directory"
     echo "      -u: Update/Create Python VirtualEnv"
     echo "      -v <>: Override/Add global variables specified in the test variables file"
 }
